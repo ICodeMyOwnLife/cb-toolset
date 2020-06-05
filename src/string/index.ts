5fb74f87a45f0e025072ad83f11712f8c93f87d7
HienTM <hientm@sendo.vn>
2020-06-05 16:36:43 +0700

    feat: add random-string

--- src/string/index.ts
+++ src/string/index.ts
@@ -2,6 +2,8 @@ const symbolSplitter = /[\s_-]/;
 
 const exhaustiveSplitter = /[\s_-]|(?=[A-Z0-9])/;
 
+const createRandomString = () => Math.random().toString(36);
+
 const createTransform = (
   separator: string,
   transformWord: (word: string) => string,
@@ -15,6 +17,18 @@ export const capitalizeWord = (word: string) =>
   !word.length ? word : word[0].toUpperCase() + word.substr(1).toLowerCase();
 
 export const isString = (u: any): u is string => typeof u === 'string';
+
+export const randomString = (length: number) => {
+  const maxLength = 8;
+  const rep = Math.floor(length / maxLength);
+  const mod = length - rep * maxLength;
+  let s = '';
+  for (let i = 0; i < rep; ++i) {
+    s += createRandomString().substr(2, maxLength);
+  }
+  if (mod) s += createRandomString().substr(2, mod);
+  return s;
+};
 
 export const toKebabCase = createTransform('-', w => w.toLowerCase());
 
